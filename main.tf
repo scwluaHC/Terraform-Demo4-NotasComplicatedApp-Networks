50c91d4f5562dd727ba5077ffda18278afd3fc2b
scwluaHC <shawn.lua@hashicorp.com>
2022-11-06 14:12:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # This external module encapsulates the VPC/Subnet creation similar to what we did earlier.
-# Modules are great for code reuse. - let's make a change to our app!!
+# Modules are great for code reuse. - let's make a change to our app!
 module "scwlua-NotasComplicatedApp-NetworkModule" {
   source  = "app.terraform.io/scwlua-test/scwlua-NotasComplicatedApp-NetworkModule/aws"
   version = "1.0.0"
